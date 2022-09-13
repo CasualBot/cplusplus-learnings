6c63f7183bbd1093216769ceabb5fbd78d7c117e
Shawn Bernard <shawn.bernard@ubisoft.com>
2022-09-13 12:32:33 -0500

    feat: finish module 3 (stream i/o & variables)

--- src/helloworld.cpp
+++ src/helloworld.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include <cstring>
 using std::cout;
+using std::cin;
 
-int main() 
+int main()
 {
-    cout << 34 << std::endl;
-}
+    cout << 32 << std::endl;
+    cout << "Test" << '\n';
+
+    int math = 2+2;
+
+    cout <<  math << std::endl;
+    int number;
+    cout << "Please enter a number: ";
+    cin >> number;
+    cout << std::endl << "You entered: " << number << std::endl;
+} 
